7763ecc7a79756aecc9e40cf59ed60e205665679
mrdbrg <rdg.brg@gmail.com>
2022-04-10 18:34:37 -0400

    set up project

---
+++ learn-webflux/src/test/java/com/reactivespring/LearnWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
