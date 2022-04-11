a33a9875b3e988e4f754d71cbb580f8684b838e6
mrdbrg <rdg.brg@gmail.com>
2022-04-10 22:08:44 -0400

    add namesFlux_map test map operator

--- learn-webflux/src/test/java/com/reactivespring/service/FluxAndMonoGeneratorServiceTest.java
+++ learn-webflux/src/test/java/com/reactivespring/service/FluxAndMonoGeneratorServiceTest.java
@@ -11,19 +11,26 @@ public class FluxAndMonoGeneratorServiceTest {
 	@Test
 	void namesFlux() {
 		
-		// given
-		
-		// when 
 		var namesFlux = fluxAndMonoGeneratorService.namesFlux();
 		
-		// then
 		StepVerifier.create(namesFlux)
 //			.expectNext("Alex", "Joye", "Mark") // expecting the exact elements
 //			.expectNextCount(3) // expecting the exact number of elements
 			.expectNext("Alex") // 1. expecting the first element 
 			.expectNextCount(2) // 2. expecting the remaining count of elements
 			.verifyComplete();
+	}
+	
+	@Test
+	void namesFlux_map() {
+		
+		var namesFlux = fluxAndMonoGeneratorService.namesFlux_map();
+		
+		StepVerifier.create(namesFlux)
+			.expectNext("ALEX", "JOYE", "MARK")
+			.verifyComplete();
 		
 	}
+	
 	
 }
