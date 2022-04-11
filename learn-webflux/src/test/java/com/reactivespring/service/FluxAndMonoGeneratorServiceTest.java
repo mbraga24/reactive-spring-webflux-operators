37d811920cbf492114adb57210c2226b3384f0b9
mrdbrg <rdg.brg@gmail.com>
2022-04-10 21:48:28 -0400

    namesFlux and nameMono test cases

---
+++ learn-webflux/src/test/java/com/reactivespring/service/FluxAndMonoGeneratorServiceTest.java
@@ -0,0 +1,29 @@
+package com.reactivespring.service;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoGeneratorServiceTest {
+
+	FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+	
+	@Test
+	void namesFlux() {
+		
+		// given
+		
+		// when 
+		var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+		
+		// then
+		StepVerifier.create(namesFlux)
+//			.expectNext("Alex", "Joye", "Mark") // expecting the exact elements
+//			.expectNextCount(3) // expecting the exact number of elements
+			.expectNext("Alex") // 1. expecting the first element 
+			.expectNextCount(2) // 2. expecting the remaining count of elements
+			.verifyComplete();
+		
+	}
+	
+}
